c91fa28e19eca37611ed8960b02b45fcde8931e0
Yuhi takahashi <mrpt79276945@gmail.com>
2017-02-18 02:32:50 +0900

    size2

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ cc.game.onStart = function(){
         document.body.removeChild(document.getElementById("cocosLoading"));
 
     cc.view.enableRetina(false);
-    cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(880, 220, cc.ResolutionPolicy.SHOW_ALL);
-    cc.view.resizeWithBrowserSize(true);
+    cc.view.adjustViewPort(false);
+    cc.view.setDesignResolutionSize(880, 720, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(false);
     cc.LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new FirstScene());
     }, this);
