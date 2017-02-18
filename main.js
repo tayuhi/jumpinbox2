c05c9b09b273d84cd36a9a652d3e5bfb7700095b
Yuhi takahashi <mrpt79276945@gmail.com>
2017-02-18 18:33:00 +0900

    size2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ cc.game.onStart = function(){
 
     cc.view.enableRetina(false);
     cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(880, 120, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(880, 420, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(false);
     cc.LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new FirstScene());
